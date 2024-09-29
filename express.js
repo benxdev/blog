36035e382bb11bbe18a3e54db13fbdfd67b74666
Benedict Uduogu <jike.exe@gmail.com>
2024-09-29 06:28:55 +0100

    First blog  commit to git

---
+++ express.js
@@ -0,0 +1,16 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 8080;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.render("page.ejs");
+});
+
+app.listen(port,  () => {
+    console.log(`Server is running on port ${port}`);
+});
